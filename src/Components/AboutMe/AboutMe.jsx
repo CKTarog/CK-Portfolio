220431073ab2b9f5916fa8d20215cc446ee995bb
CKTarog <cktarog@gmail.com>
2026-01-06 05:24:13 +0800

    Test commit from new machine

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -5,11 +5,12 @@ import Others from "./Others";
 
 const AboutMe = () => {
     return(
-        <div className="relative min-h-screen dark:bg-[#270E12]/30 bg-[#fff1dc]/30">
+        <div className="relative min-h-screen dark:bg-[#270E12]/30 bg-[#fff1dc]/30">    
         <div className="sticky top-0 left-0 right-0 z-10 flex w-full items-center space-x-4 p-2 pl-5 dark:text-[#d2b48c] text-[#733a3d] backdrop-blur-lg lg:space-x-9">
-            <Link to="/"><MdArrowBackIosNew/></Link>
+            <Link to="/"><MdArrowBackIosNew/></Link> {/*Goes back to main page*/}
             <h1 className="font-bold sm:text-2xl lg:text-4xl">About Me</h1>
         </div>
+        
         <MainAbout/>
         <div className="inline-flex w-full items-center justify-center dark:text-[#d2b48c] text-[#733a3d] pt-20">
             <hr className="bg-neutral-quaternary my-8 h-px w-64 border-0" />
