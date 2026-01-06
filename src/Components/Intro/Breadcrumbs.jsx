4d6b133ca7a6a71a7a5c469fb24a88fae2674d32
CKTarog <cktarog@gmail.com>
2026-01-06 23:41:59 +0800

    changed size of text on small screens

--- src/Components/Intro/Breadcrumbs.jsx
+++ src/Components/Intro/Breadcrumbs.jsx
@@ -1,6 +1,6 @@
 const Breadcrumbs = () => {
     return(
-        <div className="flex flex-wrap justify-center gap-2 my-4 md:text-normal text-sm *:p-1 *:md:px-3 *:px-2 *:dark:bg-[#d2b48c] *:dark:text-[#270E12] *:bg-[#733a3d] *:text-[#fff1dc] *:rounded-full sm:">
+        <div className="flex flex-wrap justify-center gap-2 my-4 md:text-lg text-[10px] *:p-1 *:md:px-3 *:px-2 *:dark:bg-[#d2b48c] *:dark:text-[#270E12] *:bg-[#733a3d] *:text-[#fff1dc] *:rounded-full sm:">
         <p> Front-End </p>
         <p> Back-End </p>
         <p> Low-code </p>
